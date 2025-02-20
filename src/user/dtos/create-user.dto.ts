cf4f807aa714139ad37aa2c384044cab69d617e0
Diones <pereiradiones987@gmail.com>
2025-02-20 16:24:26 -0300

    validando dados de entrada do body

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,7 +1,26 @@
-export  class CreateUserDto {
+import { IsEmail, IsPhoneNumber, IsString, IsStrongPassword } from "class-validator";
+import { IsCPF } from "class-validator-cpf";
+
+export class CreateUserDto {
+  
+  @IsString({ message: 'Deve ser uma string' })
   name: string;
+
+  @IsEmail({}, { message: 'Deve ser um email' })
   email: string;
+
+  @IsPhoneNumber('BR', { message: 'Deve ser um número de telefone válido' })
   phone: string;
+
+  @IsCPF({ message: 'Deve ser um CPF válido' })
   cpf: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minLowercase: 0,
+    minUppercase: 0,
+    minNumbers: 0,
+    minSymbols: 0,
+  },{ message: 'A senha não é forte o suficiente' })
   password: string;
 }
