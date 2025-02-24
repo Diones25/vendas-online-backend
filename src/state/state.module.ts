6a8119fe907e1a4a694e40f26483e6f2ced33045
Diones <pereiradiones987@gmail.com>
2025-02-24 14:12:39 -0300

    controller e service state

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { StateController } from './state.controller';
+import { StateService } from './state.service';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StateEntity } from './entities/state.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([StateEntity])],
+  controllers: [StateController],
+  providers: [StateService]
+})
 export class StateModule {}
