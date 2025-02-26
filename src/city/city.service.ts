df2525e40813110878debdbbcc622582b5c6ff9b
Diones <pereiradiones987@gmail.com>
2025-02-25 23:25:02 -0300

    implementando cache

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class CityService {
@@ -9,9 +10,15 @@ export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: any
   ) {}
 
   async getAllCitiesbyStateId(stateId: number): Promise<CityEntity[]> {
+    const citiesCached: CityEntity[] = await this.cacheManager.get(`state_${stateId}`);
+
+    if (citiesCached) {
+      return citiesCached;
+    }
 
     const cities = await this.cityRepository.find({
       where: {
@@ -22,6 +29,9 @@ export class CityService {
     if(cities.length === 0) {
       throw new NotFoundException(`Não existem cidades para o estado com o id ${stateId}`); 
     }
+
+
+    await this.cacheManager.set(`state_${stateId}`, cities);
 
     return cities;
   }
