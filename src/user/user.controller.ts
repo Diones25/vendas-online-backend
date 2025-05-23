1aa49b4cdec7e74ad386cf4c71fe35ca3eff7d5d
Diones <pereiradiones987@gmail.com>
2025-05-23 14:28:17 -0300

    atualizando regras de acesso aos endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,8 @@ import { UserService } from './user.service';
 import { ResponseUserDto } from './dtos/response-user.dto';
 import { UpdatePasswordDto } from './dtos/update-password.dto';
 import { UserId } from 'src/decorators/user-id-decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { USerType } from './enum/user-type.emu';
 
 @Controller('user')
 export class UserController {
@@ -15,12 +17,14 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @Roles(USerType.ADMIN)
   @Get()
   async findAll(): Promise<ResponseUserDto[]> {
     const users = await this.userService.findAll();
     return users.map(user => new ResponseUserDto(user));
   }
 
+  @Roles(USerType.ADMIN)
   @Get(':userId')
   async findUserById(@Param('userId') userId: number): Promise<ResponseUserDto> {
     const user = await this.userService.getUserByIdUsingRelations(userId);
@@ -30,6 +34,7 @@ export class UserController {
     return new ResponseUserDto(user);
   }
 
+  @Roles(USerType.ADMIN, USerType.USER)
   @Patch()
   async updatePassword(@UserId() id: number, @Body() updatePasswordDto: UpdatePasswordDto) {
     return this.userService.updatePassword(id, updatePasswordDto);
